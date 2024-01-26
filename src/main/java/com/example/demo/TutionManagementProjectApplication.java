59c5fdb9e5008a53fb68ae0aaca659b2d5b0f927
labaniclifav <labani.chatterjee@clifav.com>
2024-01-26 12:42:00 +0530

    Tution Management System

---
+++ src/main/java/com/example/demo/TutionManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutionManagementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutionManagementProjectApplication.class, args);
+	}
+
+}
